ca38a332e1cf1251642d501a787505914c32fbf0
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-04-28 11:53:24 +0300

    Create Challenge3_Use_Of_If_Else.java
    The file containing the use of If, else has been created and the code used.

---
+++ src/Challenge3_Use_Of_If_Else.java
@@ -0,0 +1,13 @@
+public class Challenge3_Use_Of_If_Else {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2);
+        } else if (num1 == num2) {
+            System.out.println(num1 + " is equal to " + num2);
+        }
+    }
+}
