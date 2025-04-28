330b2017ce07068b24602e422889219f42650a47
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-04-28 11:43:54 +0300

    Update Call_A_Function.java
    Added the code that calls a simple function that calculates the sum of two numbers

--- src/Call_A_Function.java
+++ src/Call_A_Function.java
@@ -1,2 +1,12 @@
 public class Call_A_Function {
+    public static int calculateSum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 3;
+        int result = calculateSum(num1, num2);
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + result);
+    }
 }
