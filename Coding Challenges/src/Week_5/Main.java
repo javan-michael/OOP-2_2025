13ab711c5d4fa837b10088af87b427eab528b531
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-05-14 10:54:39 +0300

    Update Main.java

--- Coding Challenges/src/Week_5/Main.java
+++ Coding Challenges/src/Week_5/Main.java
@@ -1,10 +1,10 @@
 public class Main {
-    // Abstraction: any action on an Account
+
     public interface Operation {
         void execute();
     }
 
-    // Base class with shared fields and a polymorphic hook
+
     public abstract static class Account {
         protected int id;
         protected String name;
@@ -20,7 +20,7 @@ public class Main {
         }
     }
 
-    // Two concrete account types
+    
     public static class Admin extends Account {
         public Admin(int id, String name) {
             super(id, name);
@@ -33,7 +33,7 @@ public class Main {
         }
     }
 
-    // Two operations demonstrating polymorphism
+
     public static class ViewOperation implements Operation {
         @Override
         public void execute() {
@@ -48,7 +48,7 @@ public class Main {
         }
     }
 
-    // Entry point
+
     public static void main(String[] args) {
         Admin admin = new Admin(1, "Alice");
         User  user  = new User(2, "Bob");
